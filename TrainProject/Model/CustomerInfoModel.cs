4d128235e25822622a7b989bea999e96766a4785
shainaguines <shainaguines@southern.edu>
2015-04-30 23:06:12 -0400

    Created Customer Info View and Model

---
+++ TrainProject/Model/CustomerInfoModel.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrainProject.Model
+{
+    class CustomerInfoModel :  INotifyPropertyChanged
+    {
+        private string firstname;
+
+        public string FirstName
+        {
+            get { return firstname; }
+            set
+            {
+                if (value != firstname)
+                {
+                    firstname = value;
+                    OnPropertyChanged("FirstNameInput");
+                }
+            }
+        }
+
+        private string lastname;
+
+        public string LastName
+        {
+            get { return lastname; }
+            set
+            {
+                if (value != lastname)
+                {
+                    firstname = value;
+                    OnPropertyChanged("LastNameInput");
+                }
+            }
+        }
+
+        private string dob;
+
+        public string DOB
+        {
+            get { return dob; }
+            set
+            {
+                if (value != dob)
+                {
+                    dob = value;
+                    OnPropertyChanged("DOBInput");
+                }
+            }
+        }
+         // this method raises PropertyChanged event
+
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null) // if there is any subscribers 
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+    
+}
